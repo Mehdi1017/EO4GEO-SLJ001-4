e2583afeea619a7a56989e1ca2b85a2781e4b9e0
Bibek Pant <pantbibek21@gmail.com>
2024-11-20 22:14:07 +0100

    updates course component to recieve data from course-wrapper component (parent)

--- src/app/view/components/card/card.component.ts
+++ src/app/view/components/card/card.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
 
 @Component({
-  selector: 'app-card',
-  templateUrl: './card.component.html',
-  styleUrls: ['./card.component.scss']
+  selector: "app-card",
+  templateUrl: "./card.component.html",
+  styleUrls: ["./card.component.scss"],
 })
 export class CardComponent implements OnInit {
+  @Input() course: {};
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    console.log(this.course);
   }
 
+  ngOnInit() {}
 }
