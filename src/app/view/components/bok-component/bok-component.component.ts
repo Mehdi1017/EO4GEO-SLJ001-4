114ab7bed9be341e23743c0f0002d4b8e3cb8aa2
Bibek Pant <pantbibek21@gmail.com>
2024-11-22 19:53:31 +0100

    create a bok-component template

---
+++ src/app/view/components/bok-component/bok-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bok-component',
+  templateUrl: './bok-component.component.html',
+  styleUrls: ['./bok-component.component.scss']
+})
+export class BokComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
