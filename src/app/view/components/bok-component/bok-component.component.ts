ffb07c4aedccf11002b04cafe4ba98106acbffd4
Mehdi1017 <mehdigasabarsa@gmail.com>
2024-11-24 20:35:18 +0100

    Retrived the id of the current selected bok element

--- src/app/view/components/bok-component/bok-component.component.ts
+++ src/app/view/components/bok-component/bok-component.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit } from "@angular/core";
-import * as bok from "@eo4geo/find-in-bok-dataviz";
+import { Component, OnInit, SimpleChanges } from '@angular/core';
+import * as bok from '@eo4geo/find-in-bok-dataviz';
 
 @Component({
-  selector: "app-bok-component",
-  templateUrl: "./bok-component.component.html",
-  styleUrls: ["./bok-component.component.scss"],
+  selector: 'app-bok-component',
+  templateUrl: './bok-component.component.html',
+  styleUrls: ['./bok-component.component.scss'],
 })
 export class BokComponentComponent implements OnInit {
   constructor() {
@@ -12,14 +12,34 @@ export class BokComponentComponent implements OnInit {
     bok.visualizeBOKData(this.inputObject);
   }
 
-  ngOnInit() {}
-
   inputObject = {
-    svgId: "#bubbles", // The ID of the SVG element where the visualization will be rendered
-    textId: "#textInfo", // The ID of the text element where information will be displayed
-    urls: ["https://eo4geo-uji.firebaseio.com/"], // An array of URLs containing the data to be visualized
-    conceptId: "GIST", // The ID of the concept to visualize (optional)
+    svgId: '#bubbles', // The ID of the SVG element where the visualization will be rendered
+    textId: '#textInfo', // The ID of the text element where information will be displayed
+    urls: ['https://eo4geo-uji.firebaseio.com/'], // An array of URLs containing the data to be visualized
+    conceptId: 'GIST', // The ID of the concept to visualize (optional)
     versions: true, // A boolean indicating whether to include versions in the visualization (optional)
     updateUrl: true, // A boolean indicating if url should be updated with the concept id (optional)
   };
+
+  onClick(): void {
+  console.log(window.location.pathname.slice(1));
+}
+
+  ngOnInit() {
+    //document.getElementsByClassName("svg-content").item(0).addEventListener("click", this.onClick);
+
+  }
+
+ /* ngAfterContentChecked() {
+    if (!this.rendered) {
+      let element = document.getElementById("bubbles");
+      if (element) {
+        console.log(element);
+        //element.addEventListener("click", this.onClick, false);
+        //this.rendered = true;
+
+        });
+      }
+  }*/
+
 }
